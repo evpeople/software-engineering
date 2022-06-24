2001de589d79d7b847a2c0ddfcdb4c14b8165ccd
Jerry's Ubuntu <jrliang@bupt.edu.cn>
2022-06-24 20:47:31 +0800

    ✨ feat(main): complete B&C Posts

--- test/main.go
+++ test/main.go
@@ -21,7 +21,7 @@ var URL string
 
 func main() {
 	// 打开json文件
-	URL = "http://122.9.146.200:8080/v1/"
+	URL = "http://122.9.146.200:8080/v1"
 	jsonFile, err := os.Open("data.json")
 
 	// 最好要处理以下错误
@@ -55,33 +55,23 @@ func main() {
 			{
 				//默认所有充电桩都是开启状态，忽略Num部分
 				pileTag, pileType := getPileTagTy(v.Id)
-				postPileStatus(pileTag, pileType)
+				sendPileReset(pileTag, pileType)
 			}
 		case "C":
 			{
-				carID := getCarID(v.Id)
-				chargeQuantity = v.Num
-				chargeType = getChargeType()
+				carIdInt, _ := strconv.Atoi(getCarID(v.Id))
+				chargeQuantity := v.Num
+				chargeType := getChargeType(v.CType)
+				sendCharge(carIdInt, chargeType, chargeQuantity)
 			}
 		}
 	}
 }
+
 func getCarID(a string) string {
 	a = a[1:]
 	return a
 }
-
-func getPileTagTy(a string) (id string, pile_type int) {
-	id = a[1:]
-	if tag := a[0]; tag == 'F' {
-		pile_type = 0
-	} else if tag == 'T' {
-		pile_type = 1
-	}
-	return
-}
-
-func postPileStatus(id string, pile_type int) {
 
 func getChargeType(a string) (ctype int) {
 	switch a[0] {
@@ -94,6 +84,17 @@ func getChargeType(a string) (ctype int) {
 	}
 	return
 }
+
+func getPileTagTy(a string) (id string, pile_type int) {
+	id = a[1:]
+	if tag := a[0]; tag == 'F' {
+		pile_type = 0
+	} else if tag == 'T' {
+		pile_type = 1
+	}
+	return
+}
+
 func stopCharge(carID string) {
 	data := make(map[string]interface{})
 	data["car_id"] = carID
@@ -102,6 +103,7 @@ func stopCharge(carID string) {
 	body, _ := io.ReadAll(resp.Body)
 	fmt.Println(string(body))
 }
+
 func sendCharge(id, typ, quantity int) {
 	data := make(map[string]interface{})
 	data["car_id"] = id
@@ -109,6 +111,16 @@ func sendCharge(id, typ, quantity int) {
 	data["charging_quantity"] = quantity
 	bytesData, _ := json.Marshal(data)
 	resp, _ := http.Post(URL+"/charge/come", "application/json", bytes.NewReader(bytesData))
+	body, _ := io.ReadAll(resp.Body)
+	fmt.Println(string(body))
+}
+
+func sendPileReset(id string, pile_type int) {
+	data := make(map[string]interface{})
+	data["id"] = id
+	data["pile_type"] = pile_type
+	bytesData, _ := json.Marshal(data)
+	resp, _ := http.Post(URL+"/admin/pile/"+id, "application/json", bytes.NewReader(bytesData))
 	body, _ := io.ReadAll(resp.Body)
 	fmt.Println(string(body))
 }
