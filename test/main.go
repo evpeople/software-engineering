41e71e577ab4f7aeb0230ba75b9df037005beb00
evpeople <hangcaihui@gmail.com>
2022-06-24 19:58:41 +0800

    ✨ feat(test/main): add test type A

--- test/main.go
+++ test/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
+	"net/http"
 	"os"
 	"strconv"
 )
@@ -15,8 +17,11 @@ type Event struct {
 	Num   int    `json:"Num"`
 }
 
+var URL string
+
 func main() {
 	// 打开json文件
+	URL = "http://122.9.146.200:8080/v1/"
 	jsonFile, err := os.Open("data.json")
 
 	// 最好要处理以下错误
@@ -37,20 +42,50 @@ func main() {
 		switch v.Type {
 		case "A":
 			{
+				charge_quantity := v.Num
 				carID := getCarID(v.Id)
-				charge_quantity = v.Num
-				charge_Type = getChargeType()
+				if charge_quantity == 0 {
+					stopCharge(carID)
+				}
+				carIdInt, _ := strconv.Atoi(carID)
+				charge_Type := getChargeType(v.CType)
+				sendCharge(carIdInt, charge_Type, charge_quantity)
 			}
 		case "B":
 		case "C":
 		}
 	}
 }
-func getCarID(a string) (id int) {
+func getCarID(a string) string {
 	a = a[1:]
-	id, err := strconv.Atoi(a)
-	if err != nil {
-		panic(err)
+	return a
+}
+func getChargeType(a string) (ctype int) {
+	switch a[0] {
+	case 'F':
+		ctype = 0
+	case 'T':
+		ctype = 1
+	case 'O':
+		ctype = 2
 	}
 	return
+}
+func stopCharge(carID string) {
+	data := make(map[string]interface{})
+	data["car_id"] = carID
+	bytesData, _ := json.Marshal(data)
+	resp, _ := http.Post(URL+"/charge/stop", "application/json", bytes.NewReader(bytesData))
+	body, _ := io.ReadAll(resp.Body)
+	fmt.Println(string(body))
+}
+func sendCharge(id, typ, quantity int) {
+	data := make(map[string]interface{})
+	data["car_id"] = id
+	data["charging_type"] = typ
+	data["charging_quantity"] = quantity
+	bytesData, _ := json.Marshal(data)
+	resp, _ := http.Post(URL+"/charge/come", "application/json", bytes.NewReader(bytesData))
+	body, _ := io.ReadAll(resp.Body)
+	fmt.Println(string(body))
 }
